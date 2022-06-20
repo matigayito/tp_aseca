c980d101b71770b07b216c46df3fe1fd7dd3dfce
matigayito <matigayito.mg@gmail.com>
2022-06-19 23:48:07 -0300

    made some appium and locust changes

--- appium/index.js
+++ appium/index.js
@@ -7,23 +7,67 @@ const opts = {
       platformName: "Android",
       platformVersion: "11",
       deviceName: "Android Emulator",
-      app: "./demo.apk",
-      appPackage: "io.appium.android.apis",
-      appActivity: ".view.TextFields",
+      app: "./wish.apk",
+      appPackage: "com.contextlogic.wish",
+      //appActivity: ".view.TextFields",
       automationName: "UiAutomator2"
     }
   };
   
   async function main () {
-    const client = await wdio.remote(opts);
-    var assert = require('assert');
+        const client = await wdio.remote(opts);
+        var assert = require('assert');
 
-    const field = await client.$("android.widget.EditText");
-    await field.setValue("Hello World!");
-    const value = await field.getText();
-    assert.strictEqual(value, "Hello World!");
+        client.setTimeouts(10000);
 
-    await client.deleteSession();
+        const item = await client.$('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/s1.a/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]');
+        item.click();
+        client.setTimeouts(10000);
+
+        const wishlistButton = await client.$('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/s1.a/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.LinearLayout[1]/android.widget.FrameLayout[2]');
+        wishlistButton.click();
+
+        const addToWL = await client.$('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout/android.widget.TextView');
+        addToWL.click();
+
+        const menuButton = await client.$('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/s1.a/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.view.ViewGroup/android.view.ViewGroup[5]');
+        menuButton.click();
+
+        const goBack = await client.$('//android.widget.ImageButton[@content-desc="Navigate up"]');
+        goBack.click();
+
+        const wishlistMenu = await client.$('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/s1.a/android.widget.FrameLayout/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[3]/android.widget.LinearLayout/android.widget.TextView');
+        wishlistMenu.click();
+
+        const wishlist = await client.$('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/s1.a/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ListView/android.widget.LinearLayout[1]');
+        wishlist.click();
+
+        const enterWL = await client.$('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/s1.a/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ListView/android.widget.LinearLayout[1]');
+        enterWL.click();
+
+        const editWL = await client.$('//android.widget.TextView[@content-desc="Edit"]');
+        editWL.click();
+
+        const selectedItem = await client.$('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/s1.a/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/androidx.viewpager.widget.ViewPager/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.ImageView');
+        selectedItem.click();
+
+        const deleteButton = await client.$('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/s1.a/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.Button[1]');
+        deleteButton.click();
+
+        const confirm = await client.$('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.Button');
+        confirm.click()
+
+
+
+
+
+
+//    const field = await client.$("android.widget.EditText");
+//    await field.setValue("Hello World!");
+//    const value = await field.getText();
+//    assert.strictEqual(value, "Hello World!");
+//
+//    await client.deleteSession();
   }
   
   main();
--- locust/locustfile.py
+++ locust/locustfile.py
@@ -3,7 +3,13 @@ from locust import HttpUser, task
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
-        self.client.get("/blitz_buy__tab")
-        self.client.get("")
-        self.client.post("", json={"username":"megayo@mail.austral.edu.ar", "password":"wishtest"})
+        self.client.post("/user", json={
+    "username": "String",
+    "password": "String",
+    "ageGroup": 1,
+    "sex": False
+})
+        self.client.get("/user")
+        self.client.get("/user/wishlist/1")
+        #self.client.post("/products/discount", json={"username":"megayo@mail.austral.edu.ar", "password":"wishtest"})
         
