d8414df1119ce54cb509c1e344dd5ac8f64301d0
agustinVon <agustin.von@ing.austral.edu.ar>
2022-06-20 01:52:28 -0300

    fixed test

--- api-wish/api/components/service/user.service.ts
+++ api-wish/api/components/service/user.service.ts
@@ -45,7 +45,7 @@ export class UserService {
     public getCategories(userId:number) {
         try {
             const userCategories = this.userRepository.getUserCategories(userId)
-            return userCategories.then(categories => Promise.all(categories.map(async category => await this.userRepository.getCategory(category.categoryId))))
+            return userCategories.then(categories => Promise.all(categories.map(async (category:any) => await this.userRepository.getCategory(category.categoryId))))
         } catch (e) {
             throw new ErrorException(ErrorCode.DatabaseError)
         }
